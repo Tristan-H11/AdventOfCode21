7ece2ab4d677abfb6f34f4017867f793e88a2d50
Tristan <triddel02@gmx.de>
2021-12-01 09:12:02 +0100

    Day1 now functional.

--- src/main/scala/Day1/Day1.scala
+++ src/main/scala/Day1/Day1.scala
@@ -5,33 +5,29 @@ import scala.io.Source
 object Day1 {
     def main(args: Array[String]): Unit = {
         val source = Source.fromFile("src/main/scala/Day1/input")
-        val lines = source.getLines().toList.map(y => y.toInt)
+        val xs = source.getLines().toList.map(_.toInt)
         source.close()
 
-        part1(lines)
-        part2(lines)
+        part1(xs)
+        part2(xs)
     }
 
     private def part1(lines: List[Int]): Unit = {
-        var result = 0
-        for (i <- 0 until lines.length - 1) {
-            if (lines(i) < lines(i + 1)) result += 1
-            //            println(lines(i) + " vs " + lines(i + 1) + (if (lines(i) < lines(i + 1)) " X" else ""))
+        val func = lines.sliding(2).count {
+            case List(a, b) => a < b
+            case _ => false
         }
-        println(result + " (1791)")
+        println(
+            func + " (1791)"
+        )
     }
 
     private def part2(lines: List[Int]): Unit = {
-        var result = 0
-        for (i <- 0 until lines.length - 1) {
-            if (getTrippleSum(lines, i) < getTrippleSum(lines, i + 1)) result += 1
-            //            println(lines(i) + " vs " + lines(i + 1) + (if (lines(i) < lines(i + 1)) " X" else ""))
+        val func: Int = lines.sliding(3).map(_.sum).toList.sliding(2).count {
+            case List(a, b) => a < b
+            case _ => false
         }
-        println(result + " (1822)")
+        println(func + " (1822)")
     }
 
-    private def getTrippleSum(lines: List[Int], index: Int): Int = {
-        if (index + 2 >= lines.length) return 0
-        lines(index) + lines(index + 1) + lines(index + 2)
-    }
 }
