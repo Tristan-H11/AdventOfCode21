1801e09a84868d25459d6b10cdb546a629ada96c
Tristan <triddel02@gmx.de>
2021-12-01 11:25:29 +0100

    Day1 scala improved.

--- src/main/scala/Day1/Day1.scala
+++ src/main/scala/Day1/Day1.scala
@@ -5,29 +5,29 @@ import scala.io.Source
 object Day1 {
     def main(args: Array[String]): Unit = {
         val source = Source.fromFile("src/main/scala/Day1/input")
-        val xs = source.getLines().toList.map(_.toInt)
+        val input = source.getLines().toList.map(_.toInt)
         source.close()
 
-        part1(xs)
-        part2(xs)
+        part1(input)
+        part2(input)
     }
 
-    private def part1(lines: List[Int]): Unit = {
-        val func = lines.sliding(2).count {
+    private def part1(xs: List[Int]): Unit = {
+        val func = xs.sliding(2).count {
             case List(a, b) => a < b
             case _ => false
         }
         println(
-            func + " (1791)"
+            func + " (Correct: 1791)"
         )
     }
 
-    private def part2(lines: List[Int]): Unit = {
-        val func: Int = lines.sliding(3).map(_.sum).toList.sliding(2).count {
+    private def part2(xs: List[Int]): Unit = {
+        val func: Int = xs.sliding(3).map(_.sum).toList.sliding(2).count {
             case List(a, b) => a < b
             case _ => false
         }
-        println(func + " (1822)")
+        println(func + " (Correct: 1822)")
     }
 
 }
