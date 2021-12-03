a30a20cd1e8277718f863056e1fdbd376ae46edc
Tristan <triddel02@gmx.de>
2021-12-03 09:36:15 +0100

    Day3 Scala:I don't know what the fuck I've done. I don't want to know what the fuck I've done. I want to die and never have a glimpse on this piece of part2 bullshit again

--- src/main/scala/Day03/Day03.scala
+++ src/main/scala/Day03/Day03.scala
@@ -5,19 +5,65 @@ import scala.io.Source
 object Day03 {
 
     def main(args: Array[String]): Unit = {
-        val source = Source.fromFile("src/main/scala/Day3/input")
-        val input = source.getLines().toList
+        val source = Source.fromFile("src/main/scala/Day03/input")
+        val input = source.getLines().toArray.map(e => e.split(""))
         source.close()
-
-        println(part1(input) + " (correct: ???)")
-        println(part2(input) + " (correct: ???)")
+        println(part1(input) + " (correct: 2250414)")
+        println(part2(input) + " (correct: 6085575)")
     }
 
-    def part1(xs: List[String]): Int = {
-        ???
+    def part1(xs: Array[Array[String]]): Int = {
+        val (high, low) = compareColumns(xs)
+        Integer.valueOf(high, 2) * Integer.valueOf(low, 2)
     }
 
-    def part2(xs: List[String]): Int = {
-        ???
+    def part2(xs: Array[Array[String]]): Int = {
+
+        var filteredList1 = xs.clone()
+        var result1: Array[String] = Array("")
+        for (i <- xs.head.indices) {
+            if (filteredList1.nonEmpty) {
+                val transposedList = filteredList1.transpose
+                val d = if (2 * transposedList(i).count(_ == "1") >= transposedList(i).length) "1" else "0"
+                filteredList1 = filteredList1.filter(e => {
+                    val digit = e(i)
+                    digit == d
+                })
+                if (filteredList1.nonEmpty)
+                    result1 = filteredList1.head
+            }
+        }
+
+        var filteredList2 = xs.clone()
+        var result2: Array[String] = Array("")
+
+        for (i <- xs.head.indices) {
+            if (filteredList2.nonEmpty) {
+                val transposedList = filteredList2.transpose
+                val d = if (2 * transposedList(i).count(_ == "1") >= transposedList(i).length) "0" else "1"
+                filteredList2 = filteredList2.filter(e => {
+                    val digit = e(i)
+                    digit == d
+                })
+                if (filteredList2.nonEmpty)
+                    result2 = filteredList2.head
+            }
+        }
+        println(result1.mkString + " " + result2.mkString)
+        Integer.valueOf(result1.mkString, 2) * Integer.valueOf(result2.mkString, 2)
+    }
+
+    def compareColumns(xs: Array[Array[String]]): (String, String) = {
+        val copy = xs.transpose
+
+        val higherBuilder = new StringBuilder()
+        val lowerBuilder = new StringBuilder()
+
+        copy.foreach(e => {
+            higherBuilder.append(if (e.count(_ == "1") * 2 >= e.length) 1 else 0)
+            lowerBuilder.append(if (e.count(_ == "1") * 2 < e.length) 1 else 0)
+        })
+
+        (higherBuilder.toString(), lowerBuilder.toString())
     }
 }
