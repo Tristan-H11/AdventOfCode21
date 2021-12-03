50f15fadf4951cea31eb1cc06932f09d7884c8cf
Tristan <triddel02@gmx.de>
2021-12-03 17:24:59 +0100

    Day3 Haskell Part1 Solution

--- src/main/scala/Day03/Day03.hs
+++ src/main/scala/Day03/Day03.hs
@@ -1,2 +1,32 @@
+import Data.List (transpose, foldl')
+import Data.Char (digitToInt)
+
 main = do
-  ls <- fmap lines (readFile "Day1/input")
+  ls <- fmap lines (readFile "Day03/input")
+  
+  print $ part1 ls
+  print $ part2 ls
+
+
+part1 :: [[Char]] -> Int
+part1 xs = (toDec $ epsilon) * (toDec $ gamma) where
+  epsilon = calcEpsilon xs
+  gamma= calcGamma xs
+
+{-
+  Part 2 missing !!
+-}
+
+calcGamma :: [[Char]] -> [Int]
+calcGamma xs = 
+  map (\x -> if(2* countLetters x '1' >= (length x)) then 1 else 0) 
+  . transpose $ xs
+
+calcEpsilon :: [[Char]] -> [Int]
+calcEpsilon xs = map (\x -> if(x==1) then 0 else 1) $ (calcGamma xs)
+
+countLetters :: String -> Char -> Int
+countLetters str c = length $ filter (== c) str  
+    
+toDec :: [Int] -> Int
+toDec = foldl' (\acc x -> acc * 2 + x) 0
